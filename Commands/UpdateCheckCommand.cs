a07ba3780063a62e3b6acc0417b8e652af77384e
Peter Ittner <peter.ittner@conplement.de>
2024-04-04 00:05:16 +0200

    feat: adds self update commands

---
+++ Commands/UpdateCheckCommand.cs
@@ -0,0 +1,48 @@
+﻿using b64.CommandFramework;
+using b64.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace b64.Commands
+{
+    class UpdateCheckCommand : Command
+    {
+        public UpdateCheckCommand(IList<string> args) : base(args)
+        {
+
+        }
+
+        public override string DescriptionLong => "Check for updates of this tool";
+        public override string DescriptionShort => "update check";
+        public override string Group => "common";
+
+
+    
+
+        public override bool Accept()
+        {
+            return Args.Count == 2 && Args[0] == "update" && Args[1] == "check";
+        }
+
+   
+
+        public override string Execute()
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = "dotnet",
+                Arguments = "tool search b64 --detail",
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+            using (var process = Process.Start(psi))
+            {
+                process.WaitForExit();
+
+                return process.StandardOutput.ReadToEnd();
+            }
+        }
+    }
+}
---
+++ Commands/UpdateInstallCommand.cs
@@ -0,0 +1,48 @@
+﻿using b64.CommandFramework;
+using b64.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace b64.Commands
+{
+    class UpdateInstallCommand : Command
+    {
+        public UpdateInstallCommand(IList<string> args) : base(args)
+        {
+
+        }
+
+        public override string DescriptionLong => "Install the latest update of this tool";
+        public override string DescriptionShort => "update install";
+        public override string Group => "common";
+
+
+    
+
+        public override bool Accept()
+        {
+            return Args.Count == 2 && Args[0] == "update" && Args[1] == "install";
+        }
+
+   
+
+        public override string Execute()
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = "dotnet",
+                Arguments = "tool update --global b64",
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+            using (var process = Process.Start(psi))
+            {
+                process.WaitForExit();
+
+                return process.StandardOutput.ReadToEnd();
+            }
+        }
+    }
+}
